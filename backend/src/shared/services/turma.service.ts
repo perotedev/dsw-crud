4f2e1167fd5cd557ae5d1a1aa6856074525cae9a
rodrigoperote <agenda@rodrigodev.online>
2021-08-10 22:07:19 -0400

    Removida importaçao nao utilizada em turma.service

--- backend/src/shared/services/turma.service.ts
+++ backend/src/shared/services/turma.service.ts
@@ -2,7 +2,6 @@ import { Turma } from '../schemas/turma.schema';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { kMaxLength } from 'buffer';
 
 @Injectable()
 export class TurmaService {
