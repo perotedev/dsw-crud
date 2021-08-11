6bfa22320323093934b9892928f18c4b204d7b0c
rodrigoperote <agenda@rodrigodev.online>
2021-08-10 21:01:12 -0400

    Adicionado expand de curso no schema de turma

--- backend/src/modules/turma.module.ts
+++ backend/src/modules/turma.module.ts
@@ -1,3 +1,4 @@
+import { CursoFicSchema } from 'src/shared/schemas/cursoFic.schema';
 import { TurmaSchema } from '../shared/schemas/turma.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TurmaService } from './../shared/services/turma.service';
@@ -19,6 +20,10 @@ import { AlunoSchema } from 'src/shared/schemas/aluno.schema';
         MongooseModule.forFeature([{
             name: 'Aluno',
             schema: AlunoSchema
+        }]),
+        MongooseModule.forFeature([{
+            name: 'CursoFic',
+            schema: CursoFicSchema
         }])
     ],
     controllers: [
