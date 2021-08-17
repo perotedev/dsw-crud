4be7e582ce58da5fc3dcda0da22fe4df34f3b0dc
TKaic <thalleskaic_@hotmail.com>
2021-08-17 01:02:55 -0400

    atualização app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,3 +1,4 @@
+import { ProfessorModule } from './modules/professor.module';
 import { TurmaModule } from './modules/turma.module';
 import { UsuarioModule } from './modules/usuario.module';
 import { Module } from '@nestjs/common';
@@ -7,6 +8,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
+    ProfessorModule,
     TurmaModule,
     UsuarioModule,
     MongooseModule.forRoot('mongodb://root:example@localhost:27017/crudDB?authSource=admin')
