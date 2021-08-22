46db91313318b8e9b65539a75e3ed8042dea5c80
rodrigoperote <agenda@rodrigodev.online>
2021-08-22 14:22:38 -0400

    Configurado banco connection para ambiente prod

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -15,19 +15,19 @@ import { MongooseModule } from '@nestjs/mongoose';
     CursoFicModule,
     UsuarioModule,
     // AMBIENTE PROD
-    // MongooseModule.forRoot('mongodb://database-crud:27027', {
-    //   dbName: "cruddb",
-    //   user: 'root',
-    //   pass: 'example',
-    //   authSource: "admin"
-    // })
-    // AMBIENTE DEV
-    MongooseModule.forRoot('mongodb://localhost:27027', {
+    MongooseModule.forRoot('mongodb://database-crud:27027', {
       dbName: "cruddb",
       user: 'root',
       pass: 'example',
       authSource: "admin"
     })
+    // AMBIENTE DEV
+    // MongooseModule.forRoot('mongodb://localhost:27027', {
+    //   dbName: "cruddb",
+    //   user: 'root',
+    //   pass: 'example',
+    //   authSource: "admin"
+    // })
   ],
   controllers: [AppController],
   providers: [AppService],
